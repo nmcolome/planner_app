fba127db518d0ba596460397d2a9ebb8f3166ecb
NataliaColome <nmcolome@gmail.com>
2020-02-11 16:37:29 -0700

    Add tests for enum & default attr.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -4,11 +4,23 @@ RSpec.describe Ingredient do
   describe 'attributes' do
     it { should respond_to :name }
     it { should respond_to :type }
-    it { should respond_to :friendly }
+    it { should respond_to :status }
   end
 
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :type }
+  end
+
+  describe 'enum' do
+    it { should define_enum_for(:type).with_values([:dairy_and_alternatives, :fruit, :vegetables, :beans, :nuts, :grains, :meats_and_poultry, :fish_and_seafood, :spices]) }
+    it { should define_enum_for(:status).with_values([:unknown, :friendly, :unfriendly]) }
+  end
+
+  describe 'default attributes' do
+    it 'status is unknown by default' do
+      ingredient = create(:ingredient)
+      expect(ingredient.status).to eq("unknown")
+    end
   end
 end
