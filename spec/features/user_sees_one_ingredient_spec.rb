92aba13354c72692ee77bb410895e1a5acf82477
NataliaColome <nmcolome@gmail.com>
2020-02-11 18:52:35 -0700

    Add spec for ingredients show page

---
+++ spec/features/user_sees_one_ingredient_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'User sees one ingredient' do
+  describe 'links from ingredients list' do
+    it "sees ingredient's details" do
+      food = create(:ingredient)
+
+      visit ingredients_path
+
+      click_on food.name
+
+      expect(current_path).to eq(ingredient_path(ingredient))
+      expect(page).to have_content(food.name)
+      expect(page).to have_content(food.category)
+      expect(page).to have_content(food.friendly)
+    end
+  end
+end
