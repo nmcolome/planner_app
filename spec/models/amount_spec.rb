160eb26c4cd6fc85de74950cbf758a651943c7d3
NataliaColome <nmcolome@gmail.com>
2020-02-14 15:12:14 -0700

    Add Amount join model spec & relationships tests

---
+++ spec/models/amount_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Amount do
+  describe 'attributes' do
+    it { should respond_to :amount } #integer
+    it { should respond_to :unit }
+    it { should respond_to :comment } #string
+    it { should respond_to :optional } #boolean
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :amount }
+    it { should validate_presence_of :unit }
+    it { should validate_presence_of :optional }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:unit).with_values([:tsp, :tbsp, :cup, :ounce, :quart, :gallon, :lb, :can, :large, :small, :medium]) }
+  end
+
+  describe 'default attributes' do
+    it 'optional is false by default' do
+      amount = create(:amount)
+      expect(amount.optional).to eq(false)
+    end
+  end
+
+  describe 'relationships' do
+    it { should belong_to :recipe }
+    it { should belong_to :ingredient }
+  end
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Recipe do
     it { should validate_presence_of :instructions }
     it { should validate_presence_of :category }
   end
+
+  describe 'enum' do
+    it { should define_enum_for(:category).with_values([:breakfasts, :soups, :salads, :sides, :sauces_and_seasonings, :salad_dressings, :snacks, :main_courses, :desserts]) }
+  end
+
+  describe 'relationships' do
+    it { should have_many :amounts }
+    it { should have_many(:ingredients).through(:amounts) }
+  end
 end
