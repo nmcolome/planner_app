7caf7955adcb69a7c5e8a82257050f75dd4a755b
NataliaColome <nmcolome@gmail.com>
2020-02-14 16:16:12 -0700

    Add recipes#show feature spec

---
+++ spec/features/user_sees_a_recipe_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'User sees a recipe' do
+  describe 'links from recipe index' do
+    it 'sees all the information related to a recipe' do
+      amount = create(:amount)
+      recipe = amount.recipe
+      ingredient = amount.ingredient
+
+      visit recipes_path
+
+      click_on recipe.title
+
+      expect(current_path).to eq(recipe_path(recipe))
+      expect(page).to have_content(recipe.title)
+      expect(page).to have_content(recipe.category)
+      expect(page).to have_content(recipe.servings)
+      expect(page).to have_content(amount.amount)
+      expect(page).to have_content(amount.unit)
+      expect(page).to have_content(ingredient.name)
+      expect(page).to have_content(ingredient.status)
+      expect(page).to have_content(recipe.instructions)
+    end
+  end
+end
